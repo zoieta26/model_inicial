51b188906aa8557235636b402f7d73f795775b5c
zoieta26 <lufeitor@gmail.com>
2026-06-05 11:10:52 -0300

    Delete TrabalhoDSO1 directory

--- TrabalhoDSO1/main.py
+++
@@ -1,5 +0,0 @@
-from controllers.controlador_principal import ControladorPrincipal
-
-if __name__ == "__main__":
-    sistema = ControladorPrincipal()
-    sistema.iniciar() 
--- TrabalhoDSO1/view/tela_relatorio.py
+++
@@ -1,9 +0,0 @@
-from view.tela_generica import TelaGenerica
-
-class TelaRelatorio(TelaGenerica):
-    def tela_opcoes(self) -> int:
-        print("\n--- RELATÓRIOS ---")
-        print("1 - Clínicas com mais atendimentos")
-        print("2 - Relatório Financeiro (Faturação por Clínica)") # <-- NOVO
-        print("0 - Voltar")
-        return self.ler_inteiro("Escolha: ", [0, 1, 2])
