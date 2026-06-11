1a322f6b450bfcbba8d0ef1521162878868a5441
zoieta26 <lufeitor@gmail.com>
2026-06-11 15:06:17 -0300

    Delete tela_relatorio.py

--- tela_relatorio.py
+++
@@ -1,8 +0,0 @@
-from view.tela_generica import TelaGenerica
-
-class TelaRelatorio(TelaGenerica):
-    def tela_opcoes(self) -> int:
-        print("\n--- RELATÓRIOS ---")
-        print("1 - Clínicas com mais atendimentos")
-        print("0 - Voltar")
-        return self.ler_inteiro("Escolha: ", [0, 1])
